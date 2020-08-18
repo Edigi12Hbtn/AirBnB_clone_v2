f9506e841fe7a053484e34e1529bb47f7790de9c
Edison Girado A <1470@Holbertonschool.com>
2020-08-18 00:28:58 -0500

    configure web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,22 +28,19 @@ def do_deploy(archive_path):
     if not exists(archive_path):
         return False
 
-    try:
-        put(archive_path, "/tmp/")
-        name_of_file = archive_path.split('/')[-1]
-        name = name_of_file.split('.')[0]  # name withouth extension.
-        final_path = "/data/web_static/releases/" + name
-        run("mkdir -p " + final_path)
-        run("tar -xzf /tmp/" + name_of_file + " -C " + final_path)
-        run("rm /tmp/" + name_of_file)
+    put(archive_path, "/tmp/")
+    name_of_file = archive_path.split('/')[-1]
+    name = name_of_file.split('.')[0]  # name withouth extension.
+    final_path = "/data/web_static/releases/" + name
+    run("mkdir -p " + final_path)
+    run("tar -xzf /tmp/" + name_of_file + " -C " + final_path)
+    run("rm /tmp/" + name_of_file)
 
-        run("mv " + final_path + "/web_static/* " + final_path)
-        run("rm -rf /data/web_static/current")
-        run("rm -rf " + final_path + "/web_static/")
-        run("ln -s {} /data/web_static/current".format(final_path))
+    run("mv " + final_path + "/web_static/* " + final_path)
+    run("rm -rf " + final_path + "/web_static/")
+    run("rm -rf /data/web_static/current")
+    run("ln -s {} /data/web_static/current".format(final_path))
 
-        disconnect_all()
-    except:
-        return False
+    disconnect_all()
 
     return True
