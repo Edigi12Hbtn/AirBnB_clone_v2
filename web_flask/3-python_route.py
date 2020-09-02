ce5cb5eac6b1d501d72ecf215f36dc4e1c9d4ff3
Edison Girado A <1470@Holbertonschool.com>
2020-09-01 21:35:40 -0500

    handling optional parameter.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Flask web application. Routes  / and /hbnb."""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """Flask Hello World."""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """Manage hbnb route."""
+    return "HBNB"
+
+@app.route('/c/<text>')
+def text_func(text):
+    """returns text in path: /c/<text>"""
+    text = text.replace('_', ' ')
+    return "{} {}".format('C', text)
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_func(text):
+    """handling with optional route."""
+    text = text.replace('_', ' ')
+    return "{} {}".format('Python', text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
