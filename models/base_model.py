58f54bfbb14d6ee1497b2582a63a6ccdf94896bc
Edison Girado A <1470@Holbertonschool.com>
2020-09-02 14:19:27 -0500

    test

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,9 @@ class BaseModel:
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-
+"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        Instatntiates a new model
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -30,6 +30,31 @@ class BaseModel:
                     val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, val)
+"""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    kwargs[key] = datetime.strptime(
+                        kwargs[key],
+                        '%Y-%m-%dT%H:%M:%S.%f'
+                        )
+                if key != "__class__":
+                    setattr(self, key, value)
+                if 'id' not in kwargs:
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs:
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs:
+                    self.updated_at = self.created_at
+                else:
+                    self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
