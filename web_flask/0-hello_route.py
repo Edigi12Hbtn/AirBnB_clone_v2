d4759b7b282c77df4606511633fd54f1d95d92d9
Edison Girado A <1470@Holbertonschool.com>
2020-09-01 21:37:40 -0500

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,7 +3,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-#app.url_map.strict_slashes = False
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -12,4 +12,4 @@ def hello_world():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
