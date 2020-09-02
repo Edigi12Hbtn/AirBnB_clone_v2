c83860eca2ad31b54bc3bbe3810c8f7ede7c3745
Edison Girado A <1470@Holbertonschool.com>
2020-09-02 13:54:31 -0500

    getter method

--- models/state.py
+++ models/state.py
@@ -3,16 +3,36 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
+type_db = getenv('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state',
-                          cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """Getter method for class attr cities."""
-        return type(self).cities
+    if type_db == 'db':  #database
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref='state',
+                              cascade="all, delete-orphan")
+
+        @property
+        def cities(self):
+            """Getter method for class attr cities."""
+            return type(self).cities
+
+    else:  # file storage
+        name = ''
+
+        @property
+        def cities(self):
+            """getter method."""
+            cities = []
+
+            for key, city in models.storage.all(City).items():
+                if city.state_id == self.id:
+                    cities.append(city)
+
+            return cities
